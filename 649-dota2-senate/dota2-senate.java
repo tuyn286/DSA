c7da375c4e115101227bba090c5410a25f99df98
Nguyễn Hoàng Tuyên <97667175+tuyn286@users.noreply.github.com>
2025-08-10 10:59:00 +0700

    Time: 12 ms (42.80%) | Memory: 44.9 MB (77.41%) - LeetSync

---
+++ 649-dota2-senate/dota2-senate.java
@@ -0,0 +1,35 @@
+class Solution {
+    public String predictPartyVictory(String senate) {
+        // vì luật xét theo thứ tự từ trái sang phải nên ai có index nhỏ hơn sẽ loại được đối thủ
+        Queue<Integer> radQueue = new ArrayDeque<>();
+        Queue<Integer> direQueue = new ArrayDeque<>();
+        int n = senate.length();
+        // luu index cua r hoac d vao queue
+        for (int i = 0; i < n; i++) {
+            if (senate.charAt(i) == 'R'){
+                radQueue.add(i);
+            } else {
+                direQueue.add(i);
+            }
+        }
+        // khi nao co phe het so luong thi tuyen bo chien thang
+        while (radQueue.size() != 0 && direQueue.size() != 0){
+            // neu co index nho hon thi loai duoc doi thu ben phai
+            if (radQueue.peek() < direQueue.peek()){
+                // loai doi thu
+                direQueue.poll();
+                // cho no vao lai voi index hon 1 vong
+                radQueue.add(radQueue.poll() + n);
+            } else {
+                radQueue.poll();
+                direQueue.add(direQueue.poll() + n);
+            }
+        }
+        // khi da co 1 phe het so luong
+        if (radQueue.size() == 0){
+            return "Dire";
+        } else {
+            return "Radiant";
+        }
+    }
+}
