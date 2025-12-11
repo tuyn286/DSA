5997250c6023a88c85ce7b473b8665f46431114b
Nguyễn Hoàng Tuyên <97667175+tuyn286@users.noreply.github.com>
2025-12-11 16:55:09 +0700

    Time: 4 ms (100.00%) | Memory: 45.6 MB (58.67%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -4,7 +4,7 @@ class Solution {
             return false;
         }
         int revert = 0, temp = x;
-        while (x > 0) {
+        while (x != 0) {
             revert = revert*10 + x%10;
             x/=10;
         }
